c56d5a99938429d6d5a550a0f4a0f4e93ccf42dc
Platy <daryl.limkz@gmail.com>
2022-10-23 17:07:51 +0800

    feat(middleware): added brief info api
    Signed-off-by: Platy <daryl.limkz@gmail.com>

--- middleware/src/utils/getinfo.js
+++ middleware/src/utils/getinfo.js
@@ -5,6 +5,31 @@ const getImageIDs = async () => {
     return ids.map((item) => item._id);
 };
 
+const getImageInfoBrief = async (id) => {
+    const info = await ImageInfoModel.findById(id, {
+        _id: 1,
+        RepoTags: 1,
+        Created: 1,
+        Author: 1,
+        Config: {
+            Hostname: 1,
+            User: 1,
+            ExposedPorts: 1,
+            Env: 1,
+            Cmd: 1,
+            Volumes: 1,
+            WorkingDir: 1,
+            Entrypoint: 1,
+        },
+        Architecture: 1,
+        Variant: 1,
+        Size: 1,
+        RootFS: 1,
+        Metadata: { LastTagTime: 1 },
+    });
+    return info;
+};
+
 const getImageInfoAll = async (id) => {
     const info = await ImageInfoModel.findById(id);
     return info;
@@ -12,5 +37,6 @@ const getImageInfoAll = async (id) => {
 
 module.exports = {
     getImageIDs: getImageIDs,
+    getImageInfoBrief: getImageInfoBrief,
     getImageInfoAll: getImageInfoAll,
 };
