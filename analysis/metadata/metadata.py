80cb0a2a391c9f5bcaddd9041643f78f72b61753
Platy <daryl.limkz@gmail.com>
2022-10-26 18:14:02 +0800

    feat(analysis): get hash and filetype
    Signed-off-by: Platy <daryl.limkz@gmail.com>

--- analysis/metadata/metadata.py
+++ analysis/metadata/metadata.py
@@ -3,10 +3,8 @@ from flask import Flask, jsonify, request
 
 import hashlib
 import tarfile
-import subprocess
 import os
-import random
-import string
+import magic
 import shutil
 
 TMP_LOC = "./tmp"
@@ -14,48 +12,47 @@ CONTAINER_DATA = "./dockerdata/container"
 app = Flask(__name__)
 
 
-def _fix_link(iden, file_path):
-    loc = ''.join(random.choice(string.ascii_lowercase) for _ in range(24))
-
-    def _extract():
-        subprocess.Popen(
-            ["tar", "-xzf", f"{CONTAINER_DATA}/{iden}.tar.gz", "-C", f"{TMP_LOC}/{loc}", file_path],
-            env=os.environ.copy(),
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-        ).communicate()
-
-    os.makedirs(f"{TMP_LOC}/{loc}")
-
-    _extract()
-
-    extracted_file = f"{TMP_LOC}/{loc}/{file_path}"
-
-    while os.path.islink(extracted_file):
-        file_path = os.path.realpath(extracted_file)[1:]
-        extracted_file = f"{TMP_LOC}/{loc}/{file_path}"
-        _extract()
-
-    with open(extracted_file, "rb") as f:
-        data = f.read()
-
-    shutil.rmtree(f"{TMP_LOC}/{loc}")
-
-    return data
-
-
-def _get_file(iden, file_path):
-    with tarfile.open(f"{CONTAINER_DATA}/{iden}.tar.gz", "r:gz") as tar:
-        if int(tar.getmember(file_path).type) != 0:
-            return _fix_link(iden, file_path)
-
-        with tar.extractfile(file_path) as f:
-            return f.read()
+def _extract(iden):
+    with tarfile.open(f"{CONTAINER_DATA}/{iden}.tar.gz", "r:gz", dereference=True) as tar:
+        tar.extractall(f"{TMP_LOC}/{iden}")
 
 
 def get_hash(iden, file_path):
-    data = _get_file(iden, file_path)
-    return hashlib.sha256(data).hexdigest()
+    loc = f"{TMP_LOC}/{iden}"
+    if not os.path.exists(loc):
+        _extract(iden)
+
+    file_path = f"{loc}/{file_path}"
+
+    if not os.path.exists(file_path):
+        return None
+
+    with open(file_path, "rb") as f:
+        return hashlib.sha256(f.read()).hexdigest()
+
+
+def get_type(iden, file_path):
+    loc = f"{TMP_LOC}/{iden}"
+    if not os.path.exists(loc):
+        _extract(iden)
+
+    file_path = f"{loc}/{file_path}"
+
+    if not os.path.exists(file_path):
+        return None
+
+    return magic.from_file(file_path)
+
+
+@app.route('/clean', methods=['GET'])
+def clean():
+    try:
+        for f in os.listdir(TMP_LOC):
+            shutil.rmtree(f"{TMP_LOC}/{f}")
+
+        return jsonify({"response": True})
+    except Exception as e:
+        return jsonify({"response": str(e)}), 500
 
 
 @app.route('/hash', methods=['GET'])
@@ -65,10 +62,19 @@ def hash():
         file_path = request.args.get("file")
         hashh = get_hash(iden, file_path)
         return jsonify({"response": hashh})
-    except KeyError:
-        return jsonify({"response": "Soft Link"}), 400
     except Exception as e:
-        return jsonify({"response": e}), 500
+        return jsonify({"response": str(e)}), 500
+
+
+@app.route('/type', methods=['GET'])
+def type():
+    try:
+        iden = request.args.get("id")
+        file_path = request.args.get("file")
+        hashh = get_type(iden, file_path)
+        return jsonify({"response": hashh})
+    except Exception as e:
+        return jsonify({"response": str(e)}), 500
 
 
 if __name__ == '__main__':
