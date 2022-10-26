00a11c1b086b2951c83bc9fc9c18e8ce740e477d
Platy <daryl.limkz@gmail.com>
2022-10-26 19:26:43 +0800

    feat(middleware): added api call to check metadata
    Signed-off-by: Platy <daryl.limkz@gmail.com>

--- middleware/src/utils/insertanal.js
+++ middleware/src/utils/insertanal.js
@@ -1,10 +1,20 @@
 const { VulnInfoModel } = require("../models/StaticAnalysisModel");
+const { FilesOnlyInfoModel } = require("../models/ContainerModel");
 
 const insertVuln = async (id, vuln) => {
     vuln._id = id;
-    return await VulnInfoModel.insertMany([vuln])
+    return await VulnInfoModel.insertMany([vuln]);
+};
+
+const insertMetadata = async (id, file, metadata) => {
+    await FilesOnlyInfoModel.updateOne({
+        $set: {
+            [`filesystem.${file}`]: { hashsum: metadata.hashsum, type: metadata.type },
+        },
+    });
 };
 
 module.exports = {
     insertVuln: insertVuln,
+    insertMetadata: insertMetadata,
 };
