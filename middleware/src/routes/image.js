75ea9b030229d0191163e3b0a5fda44af722706b
Platy <daryl.limkz@gmail.com>
2022-10-28 12:55:10 +0800

    feat(middleware): allow to download files from image layers
    Signed-off-by: Platy <daryl.limkz@gmail.com>

--- middleware/src/routes/image.js
+++ middleware/src/routes/image.js
@@ -3,6 +3,7 @@ const express = require("express");
 const router = express.Router();
 const multer = require("multer");
 const fs = require("fs");
+const axios = require("axios");
 
 const insert = require("../utils/insertinfo");
 const get = require("../utils/getinfo");
@@ -14,6 +15,8 @@ const storage = multer.diskStorage({
     },
 });
 const upload = multer({ storage: storage });
+
+const METADATA = "metadata:5000";
 
 /**
  * Get data from frontend
@@ -93,6 +96,25 @@ router.get("/fs/:id", async (req, res) => {
 
     if (fs.existsSync(path)) res.download(path);
     else res.status(500).json({ response: "File not found" });
+});
+
+// Allow file download from layer
+router.get("/fs/:id/:layerid", async (req, res) => {
+    const id = req.params.id;
+    const layerid = req.params.layerid;
+    const file = req.query.file;
+
+    try {
+        const content = await axios.get(`http://${METADATA}/layerfile`, {
+            params: { id: id, layerid: layerid, file: file },
+        });
+
+        res.setHeader("Content-Type", "application/octet-stream");
+        res.setHeader("Content-Disposition", `attachment; filename=${file}`);
+        res.send(content.data);
+    } catch (err) {
+        res.status(500).json({ response: err });
+    }
 });
 
 /**
