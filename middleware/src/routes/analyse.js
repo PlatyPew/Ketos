b2fa465e9912529914a07d7edd0465e919ef9a0a
Platy <daryl.limkz@gmail.com>
2022-11-02 23:03:35 +0800

    feat(middleware): start and stop live network capture
    Signed-off-by: Platy <daryl.limkz@gmail.com>

--- middleware/src/routes/analyse.js
+++ middleware/src/routes/analyse.js
@@ -3,7 +3,9 @@ const express = require("express");
 const router = express.Router();
 const axios = require("axios");
 
-const { SNYK } = require("../utils/ip");
+const get = require("../utils/getinfo");
+
+const { SNYK, TRAFFIC } = require("../utils/ip");
 
 // Start snyk analysis
 router.put("/vuln/:id", async (req, res) => {
@@ -13,6 +15,35 @@ router.put("/vuln/:id", async (req, res) => {
 
     const out = await axios.get(`http://${SNYK}/vulnscan`, { params: { id: id } });
     res.json(out.data);
+});
+
+// Start live capture
+router.put("/capture/start", async (req, res) => {
+    const arch = req.query.arch;
+
+    res.setHeader("Content-Type", "application/json");
+
+    try {
+        const containerIDs = await get.getContainerIDs();
+        const out = await axios.get(
+            `http://${TRAFFIC}/capture?arch=${arch}&${containerIDs.map((n) => `id=${n}`).join("&")}`
+        );
+        res.json(out.data);
+    } catch (err) {
+        res.status(500).json({ response: err });
+    }
+});
+
+// Stop live capture
+router.put("/capture/stop", async (_, res) => {
+    res.setHeader("Content-Type", "application/json");
+
+    try {
+        const out = await axios.get(`http://${TRAFFIC}/result`);
+        res.json(out.data);
+    } catch (err) {
+        res.status(500).json({ response: err });
+    }
 });
 
 module.exports = router;
