1dabeb8361d7814727c8725bd1cfa418f8242df3
Platy <daryl.limkz@gmail.com>
2022-11-05 15:46:11 +0800

    feat(frontend): use better icons
    Signed-off-by: Platy <daryl.limkz@gmail.com>

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -2,14 +2,28 @@ import React from "react";
 
 import { Icon } from "@chakra-ui/react";
 import {
-  MdBarChart,
   MdPerson,
-  MdHome,
-  MdImage,
-  MdSquareFoot,
-  MdLock,
-  //MdOutlineShoppingCart,
+  MdArchive,
+  MdStorage,
 } from "react-icons/md";
+
+import {
+  ImDownload
+} from "react-icons/im";
+
+import {
+  SiLinuxcontainers,
+} from "react-icons/si";
+
+import {
+  HiMagnifyingGlass,
+} from "react-icons/hi2";
+
+
+import {
+  FaNetworkWired,
+  FaHandPointRight,
+} from "react-icons/fa";
 
 // Admin Imports
 import MainDashboard from "views/admin/default";
@@ -20,6 +34,7 @@ import Container from "views/admin/container";
 import Network from "views/admin/network";
 import Volume from "views/admin/volume";
 import StaticAnalysis from "views/admin/staticanalysis";
+import DynamicAnalysis from "views/admin/dynamicanalysis";
 //import RTL from "views/admin/rtl";
 
 // Auth Imports
@@ -30,34 +45,34 @@ const routes = [
     name: "Acquisition",
     layout: "/admin",
     path: "/default",
-    icon: <Icon as={MdHome} width='20px' height='20px' color='inherit' />,
+    icon: <Icon as={ImDownload} width='20px' height='20px' color='inherit' />,
     component: MainDashboard,
   },
   {
     name: "Image",
     layout: "/admin",
-    icon: <Icon as={MdImage} width='20px' height='20px' color='inherit' />,
+    icon: <Icon as={MdArchive} width='20px' height='20px' color='inherit' />,
     path: "/image",
     component: Image,
   },
     {
     name: "Container",
     layout: "/admin",
-    icon: <Icon as={MdSquareFoot} width='20px' height='20px' color='inherit' />,
+    icon: <Icon as={SiLinuxcontainers} width='20px' height='20px' color='inherit' />,
     path: "/container",
     component: Container,
   },
   {
     name: "Network",
     layout: "/admin",
-    icon: <Icon as={MdLock} width='20px' height='20px' color='inherit' />,
+    icon: <Icon as={FaNetworkWired} width='20px' height='20px' color='inherit' />,
     path: "/network",
     component: Network,
   },
   {
     name: "Volume",
     layout: "/admin",
-    icon: <Icon as={MdBarChart} width='20px' height='20px' color='inherit' />,
+    icon: <Icon as={MdStorage} width='20px' height='20px' color='inherit' />,
     path: "/volume",
     component: Volume,
   },
@@ -65,8 +80,15 @@ const routes = [
     name: "Static Analysis",
     layout: "/admin",
     path: "/staticanalysis",
-    icon: <Icon as={MdPerson} width='20px' height='20px' color='inherit' />,
+    icon: <Icon as={HiMagnifyingGlass} width='20px' height='20px' color='inherit' />,
     component: StaticAnalysis,
+  },
+  {
+    name: "Dynamic Analysis",
+    layout: "/admin",
+    path: "/dynamicanalysis",
+    icon: <Icon as={FaHandPointRight} width='20px' height='20px' color='inherit' />,
+    component: DynamicAnalysis,
   },
 ];
 
